f8aadb648042af49475c4e345a7e2a14eac831b5
Dlacreme <mathieu.delacroix.fr@gmail.com>
2024-05-04 12:53:32 +0200

    working on tests and certs

---
+++ test/coroidc/rsa_store_test.exs
@@ -0,0 +1,18 @@
+defmodule Coroidc.RsaStoreTest do
+  use ExUnit.Case, async: true
+
+  describe "init_safely/0" do
+  end
+
+  describe "set_private_key/2" do
+  end
+
+  describe "get_private_key/1" do
+  end
+
+  describe "add_certificate/2" do
+  end
+
+  describe "get_certificate/1" do
+  end
+end
